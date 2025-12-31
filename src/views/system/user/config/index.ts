b93a8bff25d39e5bef8088148237d1e6dad22253
shenyang <2417254000@qq.com>
2025-12-31 14:51:57 +0800

    refactor: update user management configuration by removing userButtonConfig, consolidating action buttons in userTableConfig, and adjusting print width in Prettier settings

--- src/views/system/user/config/index.ts
+++ src/views/system/user/config/index.ts
@@ -2,7 +2,6 @@
  * 用户管理配置统一导出
  * 约定：各配置文件分开存放，这里只负责统一导出
  */
-export { userTableConfig } from './userTableConfig'
+export { userTableConfig, userActionButtons } from './userTableConfig'
 export { createUserFormConfig } from './userFormConfig'
-export { userActionButtons } from './userButtonConfig'
 export { userCrudConfig } from './userCrudConfig'
